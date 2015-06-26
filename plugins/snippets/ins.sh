d9ee6c7ade260fb775d879741bbac0f42040d082
ZRhinoceros <dlut_rhinoceros@hotmail.com>
2015-06-26 22:36:49 +0800

    add plugin vim-snippets

---
+++ plugins/snippets/ins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#main
+if [ $# -ne 1 ];then
+    echo "BAD Parameter!!!"
+    return -1
+fi
+
+PLUG_HOME=$1
+PLUG_NAME=snippets
+PLUG_SUFFIX=tar.gz
+PLUG_TARBALL="$PLUG_NAME.$PLUG_SUFFIX"
+
+mkdir -p "$PLUG_HOME/$PLUG_NAME"
+tar xzf $PLUG_TARBALL -C "$PLUG_HOME/$PLUG_NAME"
+
+return $?
